7d73753cbbad124174e37a1e089e34df539c8652
“Spencer <Spencer@bookservo.com>
2023-09-21 14:17:22 -0400

    finish enrollment association

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+    validates :name, :instructor_id, presence: true
+
+    has_many :enrolled_students,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
 end
